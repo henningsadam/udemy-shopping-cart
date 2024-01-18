19c846d84257e403b807cc74c8d9cd231461fd21
Adam Hennings <hennings.adam@gmail.com>
2024-01-18 11:12:48 -0700

    added new handlebars page for shop

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 const path = require('path');
 
 const express = require('express');
+const expressHandleBars = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.engine('handlebars', expressHandleBars())
+
 // The below configures the templating engine for the app, as well as defines the location to the views the templating engine will use
 // See app.set() in the Express.js docs for more (https://expressjs.com/en/api.html#app.set)
-app.set('view engine', 'pug') // this sets the template engine for the app to whatever your preference is here
+app.set('view engine', 'handlebars') // this sets the template engine for the app to whatever your preference is here
 app.set('views', 'views') // this tells express where to find the views to be used by templating engine
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -21,7 +24,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // res.status(404).send('<h1>Page not found</h1>');
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   res.render('404', {docTitle: 'Page Not Found'})
 });
 
